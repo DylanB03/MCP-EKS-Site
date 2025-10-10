ae0a1217db107337cc4004d8ccd14bdafa7af6d4
DylanB03 <dylanbutz@hotmail.com>
2025-10-10 09:37:39 -0400

    validation update

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class GenerateRequest(BaseModel):
     model : str = "gemini-2.5-flash"
     prompt : str
+    
+    @validator('prompt')
+    def injection_detection(cls,v):
+        if any(c in v for c in "<>[]"):
+            raise ValueError("Illegal Characters Found in Prompt")
+        return v
+    
 
 class GenerateResponse(BaseModel):
     response : str
